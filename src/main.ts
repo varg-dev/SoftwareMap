549c87b01c12ffb632693eb2c97984d6c3b02b49
Stastez <78495486+Stastez@users.noreply.github.com>
2024-02-27 15:22:55 +0100

    Automatically loads last csv on page reload if still provided by browser

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,21 @@ import {ThreeHandler} from './ThreeHandler.ts';
 import {GlyphLoader} from './GlyphLoader.ts';
 import {GuiHandler} from './GuiHandler.ts';
 
-let csv: Array<Array<string>>;
-const fileUpload = document.getElementById('fileUpload') as HTMLInputElement;
-fileUpload.addEventListener('change', async () => {
-	const fileList = fileUpload.files;
-	if (!fileList) return;
-	const file = fileList[0];
-	if (!file) return;
-	csv = await parse(await file.text());
-	await threeHandler.sceneHandler.setCsv(csv);
-});
-
 const threeHandler = new ThreeHandler();
 const glyphLoader = new GlyphLoader(threeHandler.sceneHandler);
 new GuiHandler(threeHandler, glyphLoader);
+
+const fileUpload = document.getElementById('fileUpload') as HTMLInputElement;
+
+await checkAndLoadCsv();
+
+fileUpload.addEventListener('change', checkAndLoadCsv);
+
+async function checkAndLoadCsv() {
+	const fileList = fileUpload.files;
+	if (fileList === null) return;
+	const file = fileList[0];
+	if (file === undefined) return;
+	const csv = await parse(await file.text());
+	await threeHandler.sceneHandler.setCsv(csv);
+}
