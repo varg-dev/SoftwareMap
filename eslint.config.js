e84aac4c605faddadddf35eb8808d4871c7efed6
stastez <78495486+Stastez@users.noreply.github.com>
2024-07-11 19:44:39 +0200

    Bump deps

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import tseslint from "typescript-eslint";
 
 
 export default [
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
